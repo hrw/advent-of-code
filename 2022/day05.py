5fcd55116ac2ad57d76f7884f7c73fd203754cb6
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2022-12-05 11:15:51 +0100

    grammar is a beach

--- 2022/day05.py
+++ 2022/day05.py
@@ -6,26 +6,26 @@ dane_dnia = załaduj_dane("05")
 
 wynik1 = ""
 
-ilość_słupków = 0
+ile_słupków = 0
 słupki = {}
 już_ruchy = False
 
 for linia in dane_dnia:
 
-    if not ilość_słupków:
-        ilość_słupków = int(len(linia) / 4)
-        for słupek in range(1, 1 + ilość_słupków):
+    if not ile_słupków:
+        ile_słupków = int(len(linia) / 4)
+        for słupek in range(1, 1 + ile_słupków):
             słupki[słupek] = []
 
     if linia.startswith(" 1 "):
         już_ruchy = True
-        for słupek in range(1, 1 + ilość_słupków):
+        for słupek in range(1, 1 + ile_słupków):
             słupki[słupek].reverse()
             słupki[słupek] = list(filter(str.strip, słupki[słupek]))
         continue
 
     if not już_ruchy:
-        for słupek in range(1, 1 + ilość_słupków):
+        for słupek in range(1, 1 + ile_słupków):
             potencjalna_litera = linia[1 + (słupek - 1) * 4: -2 + słupek * 4]
             słupki[słupek].append(potencjalna_litera)
     elif linia.startswith("\n"):
